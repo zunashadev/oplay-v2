50935171e2c6863c0e85066c2f8b81ea622a89dd
Zun Asha <zunasha080602@gmail.com>
2025-04-16 01:28:45 +0700

    update

--- src/views/customer/partials/Notification.vue
+++ src/views/customer/partials/Notification.vue
@@ -1,10 +1,14 @@
-<script setup></script>
+<script setup>
+import CrossIcon from '@/components/icons/Cross.vue';
+</script>
 
 <template>
   <div class="rounded-md border border-red-500 bg-red-500/10 px-5 py-3 text-red-500">
     <div class="flex items-center justify-between gap-5">
       <p class="text-sm">Akun xxxxxx akan berakhir pada 2 hari lagi.</p>
-      <div class="">Close</div>
+      <div class="transition-all hover:cursor-pointer hover:text-red-600">
+        <CrossIcon class="size-5" />
+      </div>
     </div>
   </div>
 </template>
