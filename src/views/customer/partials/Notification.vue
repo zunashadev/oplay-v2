ceeb6a043e9808a1ef82a7a42bbea82e028f1b59
Zun Asha <zunasha080602@gmail.com>
2025-04-16 01:27:24 +0700

    update

--- src/views/customer/partials/Notification.vue
+++ src/views/customer/partials/Notification.vue
@@ -2,9 +2,9 @@
 
 <template>
   <div class="rounded-md border border-red-500 bg-red-500/10 px-5 py-3 text-red-500">
-    <div class="flex items-center justify-between">
-      <p>Akun xxxxxx akan berakhir pada 2 hari lagi.</p>
-      <div>Close</div>
+    <div class="flex items-center justify-between gap-5">
+      <p class="text-sm">Akun xxxxxx akan berakhir pada 2 hari lagi.</p>
+      <div class="">Close</div>
     </div>
   </div>
 </template>
