260b9b94fb08b31554898804ab163c4a1ef604d6
Zun Asha <zunasha080602@gmail.com>
2025-04-25 23:46:29 +0700

    update

--- src/views/customer/partials/ActiveAccountList.vue
+++ src/views/customer/partials/ActiveAccountList.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="flex flex-col gap-5 rounded-xl bg-gray-900 px-5 py-5">
+  <div class="flex flex-col gap-5 rounded-xl bg-gray-800 px-5 py-5">
     <div class="flex items-center justify-between">
       <div class="flex items-center gap-3">
         <div class="h-6 w-1 rounded-md bg-yellow-500"></div>
