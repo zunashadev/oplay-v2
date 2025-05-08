9a16075a8644d440e99227976707a96852061f84
Zun Asha <zunasha080602@gmail.com>
2025-05-08 19:19:00 +0700

    update

--- src/components/discounts/DiscountPrice.vue
+++ src/components/discounts/DiscountPrice.vue
@@ -31,27 +31,26 @@ const finalPrice = computed(() =>
 
 <template>
   <div class="flex items-center gap-1">
+    <!-- Has Discount -->
     <template v-if="hasDiscount">
-      <p class="text-xs font-normal text-gray-500 line-through">
-        {{ formatRupiah(price) }}
-      </p>
-      <p class="text-xs font-normal text-yellow-500 sm:text-base">
-        {{ formatRupiah(finalPrice) }}
-      </p>
-      <span
-        v-if="discountType === 'fixed_amount'"
-        class="ml-1 flex-none rounded-sm bg-red-500 px-1 text-xs text-white sm:ml-2 sm:px-1.5"
-      >
-        -{{ formatRupiah(discountValue) }}
-      </span>
-      <span
-        v-else-if="discountType === 'percentage'"
-        class="ml-1 flex-none rounded-sm bg-red-500 px-1 text-xs text-white sm:ml-2 sm:px-1.5"
-      >
-        -{{ discountValue }}%
+      <div class="flex flex-col items-center sm:flex-row sm:gap-2">
+        <!-- Normal Price -->
+        <p class="text-xs font-normal text-gray-500 line-through">
+          {{ formatRupiah(price) }}
+        </p>
+        <!-- Final Price -->
+        <p class="text-xs font-normal text-yellow-500 sm:text-base">
+          {{ formatRupiah(finalPrice) }}
+        </p>
+      </div>
+      <!-- Discount -->
+      <span class="ml-1 flex-none rounded-sm bg-red-500 px-1 text-xs text-white sm:ml-2 sm:px-1.5">
+        <span v-if="discountType === 'fixed_amount'"> -{{ formatRupiah(discountValue) }} </span>
+        <span v-else-if="discountType === 'percentage'">-{{ discountValue }}%</span>
       </span>
     </template>
 
+    <!-- No Discount -->
     <template v-else>
       <p class="text-xs font-normal text-yellow-500 sm:text-base">
         {{ formatRupiah(price) }}
